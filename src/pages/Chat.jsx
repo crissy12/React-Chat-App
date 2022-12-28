28ca52226ebe9d4552b6d395205b1334ad63e9d3
Cristian <cristiangcs45@gmail.com>
2022-12-28 17:07:39 -0500

    Further work on the main chat.

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -4,12 +4,16 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { allUsersRoute } from "../utils/APIRoutes";
 import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
+import ChatContainer from "../components/ChatContainer";
 
 function Chat() {
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [currentChat, setCurrentChat] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     if (!localStorage.getItem("chat-app-user")) {
       navigate("/login");
@@ -19,6 +23,7 @@ function Chat() {
 
     async function setUser() {
       setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+      setIsLoaded(true);
     }
   }, []);
 
@@ -49,6 +54,11 @@ function Chat() {
           currentUser={currentUser}
           changeChat={handleChatChange}
         />
+        {isLoaded && currentChat === undefined ? (
+          <Welcome currentUser={currentUser} />
+        ) : (
+          <ChatContainer currentChat={currentChat} />
+        )}
       </div>
     </Container>
   );
